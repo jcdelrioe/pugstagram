e46b4a0393c59200aa30ac66f28c7f28430d38a9
Juan Carlos del Rio <jcdre@hotmail.com>
2021-01-22 14:02:38 -0500

    pugstagram

---
+++ src/components/Profile.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let nickname;
+  export let name;
+</script>
+
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img
+        src="https://lh3.googleusercontent.com/ogw/ADGmqu9c092rxbVDNs46Z96V6rfvVyagI4iBgxtoR0LRmoU=s32-c-mo"
+        alt="JC Foto"
+      />
+    </div>
+    <div class="Profile-info">
+      <h2>{nickname}</h2>
+      <span>{name}</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Profile from "./Profile.svelte";
+  import Stories from "./Stories.svelte";
+  import Footer from "./Footer.svelte";
+
+  export let nickname;
+  export let name;
+</script>
+
+<div class="Sidebar">
+  <div class="Sidebar-container">
+    <Profile nickname="jcdelrioe" name="Juan Carlos Del Rio" />
+    <Stories />
+    <Footer />
+  </div>
+</div>
+
+<style>
+  .Sidebar {
+    position: relative;
+    padding: 4.5em 0 0 0;
+  }
+  .Sidebar-container {
+    position: fixed;
+  }
+</style>

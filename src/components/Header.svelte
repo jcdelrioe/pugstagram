67a7a013ffe43ae7afd7fe880c6d82cf0c86f926
Juan Carlos del Rio <jcdre@hotmail.com>
2021-01-25 09:55:05 -0500

    service worker, manifest

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,18 @@
 <script>
+  import { goto } from "@sapper/app";
   import { likeCount } from "../store/store.js";
   export let segment;
+
+  const navigateHome = async () => {
+    await goto("/");
+  };
 </script>
 
 <div class="Header">
   <div class="Header-container">
     <div class="Header-content">
       <div class="Header-logo">
-        <h1>Pugstagram</h1>
+        <h1 on:click={navigateHome}>Pugstagram</h1>
       </div>
       <div class="Header-nav">
         <ul>
@@ -18,6 +23,7 @@
           <li>
             <a
               aria-current={segment === "profile" ? "page" : undefined}
+              rel="prefetch"
               href="profile">
               <i class="fas fa-user-alt" />
             </a>
